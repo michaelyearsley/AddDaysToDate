effe43f35c48b852f6ef88dbe92b1a7e45f1f9c9
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-02-06 23:40:33 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace AddDaysToDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Take user input into a varriable and covert it to int
+            Console.Write("month: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("Day: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.Write("Year (YYYY): ");
+            //int year = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+            //Reads out user input
+            Console.WriteLine("The date you entered date mm/dd/yyyy: {0}/{1}/{2}", month, day, year);
+            // Takes user input for amout of days covert to int
+
+            Console.Write("Enter numbers of days you want to add: ");
+            //int addDay = int.Parse(Console.ReadLine());
+            int addDay = Sup.FindaddDay(int.Parse(Console.ReadLine()), month, day, year);
+            year = Sup.FindYear(year, addDay);
+
+            //Sup.adddaytoDate(day, month, year, addDay);
+
+
+            // finds months with 31
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+
+                if (31 < (day + addDay) && month < 12)
+                {
+                    month = (month + addDay);
+                    day = 1;
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+                else if (31 < (day + addDay) && month == 12)
+                {
+                    month = 1;
+                    day = 1;
+                    year = (year + 1);
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+                else if (31 == (day + addDay))
+                {
+                    day = 31;
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+                else if (31 > (day + addDay))
+                {
+
+                }
+            }
+
+            // finds months with 30
+            if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+
+                if (30 < (day + addDay))
+                {
+                    month = (month + 1);
+                    day = 1;
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+
+                }
+                else if (30 <= (day + addDay))
+                {
+                    day = 30;
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+                else if (30 > (day + addDay))
+                {
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+
+            }
+
+            // finds months with 29
+            if (month == 2)
+            {
+
+                if (29 < (day + addDay))
+                {
+                    month = (month + 1);
+                    day = 1;
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+                else if (29 > (day + addDay))
+                {
+                    day = day + addDay;
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+                else if (29 == (day + addDay))
+                {
+                    day = 29;
+                    Console.WriteLine("{0}/{1}/{2}", month, day, year);
+                }
+
+            }
+        }
+    }
+}
---
+++ Sup.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddDaysToDate
+{
+    class Sup
+    {
+        public static int FindaddDay(int addDay, int day, int month, int year)
+        {
+            int x = Sup.dayCalculator(addDay, day, month, year);
+            if (x % 365 == 0)
+            {
+                addDay = 0;
+            }
+            else if (x % 365 >= 0)
+            {
+                addDay = (x % 365);
+            }
+            else
+            {
+
+                return addDay;
+            }
+            return addDay;
+        }
+        public static int FindYear(int year, int addDay)
+        {
+            if (addDay >= 365)
+            {
+                if ((addDay % 365) == 0)
+                {
+                    year = year + (addDay / 365);
+                }
+                else
+                {
+                    year = year + (Convert.ToInt32(addDay / 365));
+                }
+                addDay = addDay % 365;
+            }
+            return year;
+        }
+        public static int dayCalculator(int day, int month, int year, int addDay)
+        {
+
+            int x = addDay + day;
+            switch (month - 1)
+            {
+                case 1:
+                    x = x + 31;
+                    break;
+                case 2:
+                    if (year % 4 == 0)
+                        x = x + 60;
+                    else
+                        x = x + 59;
+                    break;
+                case 3:
+                    if (year % 4 == 0)
+                        x = x + 91;
+                    else
+                        x = x + 90;
+                    break;
+                case 4:
+                    if (year % 4 == 0)
+                        x = x + 121;
+                    else
+                        x = x + 120;
+                    break;
+                case 5:
+                    if (year % 4 == 0)
+                        x = x + 152;
+                    else
+                        x = x + 151;
+                    break;
+                case 6:
+                    if (year % 4 == 0)
+                        x = x + 182;
+                    else
+                        x = x + 181;
+                    break;
+                case 7:
+                    if (year % 4 == 0)
+                        x = x + 213;
+                    else
+                        x = x + 212;
+                    break;
+                case 8:
+                    if (year % 4 == 0)
+                        x = x + 244;
+                    else
+                        x = x + 243;
+                    break;
+                case 9:
+                    if (year % 4 == 0)
+                        x = x + 274;
+                    else
+                        x = x + 273;
+                    break;
+                case 10:
+                    if (year % 4 == 0)
+                        x = x + 305;
+                    else
+                        x = x + 304;
+                    break;
+                case 11:
+                    if (year % 4 == 0)
+                        x = x + 335;
+                    else
+                        x = x + 334;
+                    break;
+                case 12:
+                    if (year % 4 == 0)
+                        x = x + 336;
+                    else
+                        x = x + 335;
+                    break;
+            }
+            return x;
+        }
+        //public static int FindMonth(int day, int month, int year, int addDay)
+        //    if 
+        //    switch (y)
+        //    {
+        //        case 1:
+        //            x = x + 31;
+        //            break;
+        //        case 2:
+        //            if (year % 4 == 0)
+        //                x = x + 60;
+        //            else
+        //                x = x + 59;
+        //            break;
+        //        case 3:
+        //            if (year % 4 == 0)
+        //                x = x + 91;
+        //            else
+        //                x = x + 90;
+        //            break;
+        //        case 4:
+        //            if (year % 4 == 0)
+        //                x = x + 121;
+        //            else
+        //                x = x + 120;
+        //            break;
+        //        case 5:
+        //            if (year % 4 == 0)
+        //                x = x + 152;
+        //            else
+        //                x = x + 151;
+        //            break;
+        //        case 6:
+        //            if (year % 4 == 0)
+        //                x = x + 182;
+        //            else
+        //                x = x + 181;
+        //            break;
+        //        case 7:
+        //            if (year % 4 == 0)
+        //                x = x + 213;
+        //            else
+        //                x = x + 212;
+        //            break;
+        //        case 8:
+        //            if (year % 4 == 0)
+        //                x = x + 244;
+        //            else
+        //                x = x + 243;
+        //            break;
+        //        case 9:
+        //            if (year % 4 == 0)
+        //                x = x + 274;
+        //            else
+        //                x = x + 273;
+        //            break;
+        //        case 10:
+        //            if (year % 4 == 0)
+        //                x = x + 305;
+        //            else
+        //                x = x + 304;
+        //            break;
+        //        case 11:
+        //            if (year % 4 == 0)
+        //                x = x + 335;
+        //            else
+        //                x = x + 334;
+        //            break;
+        //        case 12:
+        //            if (year % 4 == 0)
+        //                x = x + 336;
+        //            else
+        //                x = x + 335;
+        //            break;
+
+
+
+        //    }
+
+       
+            
+        
+    }
+}
